729270e529cc22dcabf7ad8cf139572713a0c75e
Wayne881004 <t0919247347@gmail.com>
2019-11-18 15:04:15 +0800

    2018.11.18 modified

--- app/src/main/java/com/example/meilolbook/ui/popular/PopularFragment.kt
+++ app/src/main/java/com/example/meilolbook/ui/popular/PopularFragment.kt
@@ -1,4 +1,4 @@
-package com.example.meilolbook.ui.Popular
+package com.example.meilolbook.ui.popular
 
 import android.os.Bundle
 import android.view.LayoutInflater
--- app/src/main/java/com/example/meilolbook/ui/popular/PopularViewModel.kt
+++ app/src/main/java/com/example/meilolbook/ui/popular/PopularViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.meilolbook.ui.Popular
+package com.example.meilolbook.ui.popular
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
