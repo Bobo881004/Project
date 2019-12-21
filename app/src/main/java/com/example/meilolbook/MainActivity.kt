865c7702473b0132f9e2faa868b7e6f3eb253e87
Wayne881004 <t0919247347@gmail.com>
2019-12-22 00:21:06 +0800

    delete popular

--- app/src/main/java/com/example/meilolbook/MainActivity.kt
+++ app/src/main/java/com/example/meilolbook/MainActivity.kt
@@ -396,8 +396,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_search, R.id.nav_statistics,
-                R.id.nav_popular
+                R.id.nav_home, R.id.nav_search, R.id.nav_statistics
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
